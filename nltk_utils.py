e14a39dc8d52a733622b7da3525b80b345d40b6b
Hemal99 <h.githmin@gmail.com>
2023-03-02 22:25:47 +0530

    update

--- nltk_utils.py
+++ nltk_utils.py
@@ -4,17 +4,6 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
-nltk.download()
-
 
 def tokenize(sentence):
     """
